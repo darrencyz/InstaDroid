9ebf8a6c329e987afd21757c3d3f736f37fc8bec
Darren Cao <darrencaoyz@gmail.com>
2016-04-01 20:38:03 -0400

    add landscape

--- app/src/main/java/com/dcaoyz/fotag/View.java
+++ app/src/main/java/com/dcaoyz/fotag/View.java
@@ -1,9 +1,8 @@
 package com.dcaoyz.fotag;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.view.ViewGroup;
-import android.widget.GridLayout;
-import android.widget.GridView;
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
@@ -28,28 +27,49 @@ public class View extends LinearLayout implements Observer {
 
     @Override
     public void update(Observable observable, Object data) {
-        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.contentmain);
-        viewGroup.removeAllViews();
-
         if (model.collection.size() == 0) {
             pictures = new ArrayList<ViewImage>();
         }
 
         else if (model.collection.size() > pictures.size()) {
             for (int i = pictures.size(); i < model.collection.size(); i++ ) {
-                //ViewImage imageView = model.collection.get(i).getImage();
                 ViewImage imageView = new ViewImage(getContext(), model.collection.get(i), model);
                 pictures.add(imageView);
             }
         }
 
-        for (int i = 0; i < model.collection.size(); i++) {
-            if (model.collection.get(i).rating >= model.filter) {
-                viewGroup.addView(pictures.get(i));
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            ViewGroup viewGroup1 = (ViewGroup) findViewById(R.id.firstLayout);
+            ViewGroup viewGroup2 = (ViewGroup) findViewById(R.id.thirdLayout);
+            viewGroup1.removeAllViews();
+            viewGroup2.removeAllViews();
+
+            boolean left = true;
+
+            for (int i = 0; i < model.collection.size(); i++) {
+                if (model.collection.get(i).rating >= model.filter) {
+                    ViewGroup currentGroup;
+                    if (left) {
+                        currentGroup = (ViewGroup) findViewById(R.id.firstLayout);
+                        left = false;
+                    }
+                    else {
+                        currentGroup = (ViewGroup) findViewById(R.id.thirdLayout);
+                        left = true;
+                    }
+                    currentGroup.addView(pictures.get(i));
+                }
             }
         }
+        else {
+            ViewGroup viewGroup = (ViewGroup) findViewById(R.id.contentmain);
+            viewGroup.removeAllViews();
 
-        //GridView gridView = (GridView) findViewById(R.id.gridView);
-        //gridView.setAdapter();
+            for (int i = 0; i < model.collection.size(); i++) {
+                if (model.collection.get(i).rating >= model.filter) {
+                    viewGroup.addView(pictures.get(i));
+                }
+            }
+        }
     }
 }
